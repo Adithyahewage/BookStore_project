03a56f57038d75841f1a0cf9d0218a97a91461d0
adithyahewage <adithyahewage69@gmail.com>
2024-11-13 02:01:14 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+'mongodb+srv://adithyaght:wxSc2oHl41QTKz2T@bookstoredb.4c3hm.mongodb.net/book-collection?retryWrites=true&w=majority&appName=BookStoreDB'
+
